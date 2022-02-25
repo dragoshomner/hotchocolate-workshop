5feeb330342acd2ea030369f3c90020a7f1f180b
EUROPE\dragoshomner <dragoshomner@microsoft.com>
2022-02-25 15:22:40 +0100

    sessions 1, 2 and 3

---
+++ GraphQL/Extensions/UseApplicationDbContextAttribute.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Data;
+using HotChocolate.Types.Descriptors;
+using System.Reflection;
+
+namespace GraphQL.Extensions
+{
+    public class UseApplicationDbContextAttribute : ObjectFieldDescriptorAttribute
+    {
+        public override void OnConfigure(
+            IDescriptorContext context,
+            IObjectFieldDescriptor descriptor,
+            MemberInfo member)
+        {
+            descriptor.UseDbContext<ApplicationDbContext>();
+        }
+    }
+}
---
+++ GraphQL/Graphql/Types/AddSpeakerPayload.cs
@@ -0,0 +1,14 @@
+﻿using GraphQL.Data;
+
+namespace GraphQL.Types
+{
+    public class AddSpeakerPayload
+    {
+        public Speaker Speaker { get; }
+
+        public AddSpeakerPayload(Speaker speaker)
+        {
+            Speaker = speaker;
+        }
+    }
+}
